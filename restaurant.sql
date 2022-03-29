a178f28536fd6c7f631086a1da54a122cadd1f2e
chloeWieser <chloerosewieser@gmail.com>
2022-03-29 19:21:53 -0400

    hmk

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 --2. create table
 
 -- CREATE TABLE restaurant (
-    
+
 --     id serial primary key,
 --     name varchar,
 --     distance integer,
@@ -73,3 +73,155 @@
 -- get the max stars of a restaurant by category.
 -- SELECT MAX(stars) FROM restaurant;
 -- ??? how do you do it for all categories at once?
+
+
+
+
+-- Restaurant DB V2
+
+--1.
+--created database in command line like this: postgres@whiteRose:~$ createdb restaurantv2
+
+
+--2. create tables:
+
+-- CREATE TABLE restaurant (
+--     id serial primary key,
+--     name varchar,
+--     address varchar,
+--     category varchar
+-- );
+
+-- CREATE TABLE reviewer (
+--     id serial primary key,
+--     name varchar,
+--     email varchar,
+--     karma integer CHECK (karma >= 0 and karma < 8)
+-- );
+
+-- CREATE TABLE review (
+--     id serial primary key,
+--     reviewer_id integer REFERENCES reviewer (id),
+--     stars integer CHECK (stars > 0 and stars < 6),
+--     title varchar,
+--     review varchar,
+--     restaurant_id integer REFERENCES restaurant (id)
+-- );
+
+
+
+
+--3 Insert Data
+
+-- INSERT INTO restaurant VALUES 
+-- (DEFAULT, 'Olive Garden', '1234 Almond St Houston TX', 'Italian'),
+-- (DEFAULT, 'Torchys', '2341 Peanut St Houston TX', 'Tex-Mex'),
+-- (DEFAULT, 'Franks Grill', '3412 Walnut Bend Houston TX', 'American'),
+-- (DEFAULT, 'Outback Steakhouse', '4123 Pecan Rd Houston TX',  'American'),
+-- (DEFAULT, 'Pappas BBQ', '4123 Beanwi Rd Houston TX', 'BBQ'),
+-- (DEFAULT, 'Raising Canes', '3124 Pelbri Rd Houston TX', 'Fried Chicken'),
+-- (DEFAULT, 'Garden of Olives', '1231 Groot St Houston TX', 'Italian'),
+-- (DEFAULT, 'Flaming Tacos', '2342 Ole St Houston TX', 'Tex-Mex'),
+-- (DEFAULT, 'Chucks Grill', '3413 Latrick Bend Houston TX', 'American'),
+-- (DEFAULT, 'Salt Grass Steakhouse', '4124 Shuum Rd Houston TX',  'American'),
+-- (DEFAULT, 'Hidaway BBQ', '4125 Swantro Rd Houston TX', 'BBQ'),
+-- (DEFAULT, 'Chick-fil-a', '3126 Helm Rd Houston TX', 'Fried Chicken');
+
+-- INSERT INTO reviewer VALUES
+-- (DEFAULT, 'Karen', 'tookaren@hotmail.com', 2),
+-- (DEFAULT, 'Billy', 'billywilly@hotmail.com', 3),
+-- (DEFAULT, 'Bobby', 'bobbynewport@hotmail.com', 0),
+-- (DEFAULT, 'Samantha', 'sam185@hotmail.com', 7),
+-- (DEFAULT, 'Krieg', 'psychokrieg@hotmail.com', 1),
+-- (DEFAULT, 'Charles', 'charleschuck@hotmail.com', 4),
+-- (DEFAULT, 'Matt', 'magicmatt@hotmail.com', 6),
+-- (DEFAULT, 'Rick', 'rick@hotmail.com', 5);
+
+-- INSERT INTO review VALUES
+-- (DEFAULT, 1, 1, 'Sooo Rude...', 'The employees were so rude to me.', 1),
+-- (DEFAULT, 2, 3.5, 'Great Food', 'Food was good, atmosphere was OK', 2),
+-- (DEFAULT, 3, 5, 'It was OK', 'It was OK. Food was not too great', 3),
+-- (DEFAULT, 4, 2.5, 'Kinda Rude', 'Food was great. Employees were kinda rude.', 4),
+-- (DEFAULT, 5, 4.5, 'Amazing', 'The food was sooo good. I have to go again.', 5),
+-- (DEFAULT, 6, 3.5, 'Not my style but good food.', 'Atmosphere was not for me but the food was good.', 6),
+-- (DEFAULT, 7, 4, 'Slow service', 'Service was slow but food was good when we got it.', 7),
+-- (DEFAULT, 8, 3, 'Great Experience', 'Food was great and employees were so nice.', 8),
+-- (DEFAULT, 1, 2.5, 'Would not go again', 'Restaurant was dirty and employees were rude. Food was OK.', 9),
+-- (DEFAULT, 2, 2, 'Have to go again.', 'Great food. Wonderfull experience and atmosphere.', 10),
+-- (DEFAULT, 3, 5, 'Why???', 'My only complaint is that this restaurant is on the other side of town.', 11),
+-- (DEFAULT, 4, 4, 'How much food can you eat', 'Food was great. Portions were big.', 12),
+-- (DEFAULT, 5, 3, 'Could not eat another bite', 'I was so full when I left. Gotta go again.', 1),
+-- (DEFAULT, 6, 1, 'Not my cup of tea', 'They did not have my kind of tea.', 2),
+-- (DEFAULT, 7, 2, 'Very very rude', 'Employee at the cashier yelled at me. Food was OK.', 3),
+-- (DEFAULT, 8, 1.5, 'Not very good', 'Food and atmosphere was not very good.', 4);
+
+--4
+
+--List all the reviews for a given restaurant given a specific restaurant ID.
+-- SELECT * 
+-- FROM review 
+-- WHERE restaurant_id = 1;
+
+
+--List all the reviews for a given restaurant, given a specific restaurant name.
+-- SELECT review, restaurant.name
+-- FROM review
+-- INNER JOIN restaurant ON review.restaurant_id = restaurant.id
+-- WHERE name = 'Olive Garden';
+
+
+--List all the reviews for a given reviewer, given a specific author name.
+-- SELECT review, name, title
+-- FROM review
+-- INNER JOIN reviewer ON review.reviewer_id = reviewer.id   --so far we have: show [whatever columns specified in SELECT]] from review and reviewer tables- synced up by the reviewer ID
+-- WHERE name = 'Bobby';
+
+
+--List all the reviews along with the restaurant they were written for. In the query result, select the restaurant name and the review text.
+-- ?DON'T UNDERSTAND THE QUESTION
+
+-- SELECT review, restaurant.name FROM(
+--     SELECT *
+--     FROM review
+--     INNER JOIN restaurant ON review.restaurant_id = restaurant.id
+-- ) as restaurantReview;
+
+
+
+--Get the average stars by restaurant. The result should have the restaurant name and its average star rating.
+
+-- SELECT AVG(stars), restaurant.name
+-- FROM review
+-- INNER JOIN restaurant ON review.restaurant_id = restaurant.id
+-- GROUP BY restaurant.name;
+
+
+
+--Get the number of reviews written for each restaurant. The result should have the restaurant name and its review count.
+
+-- SELECT COUNT(review), restaurant.name 
+-- FROM review
+-- INNER JOIN restaurant ON review.restaurant_id = restaurant.id
+-- GROUP BY restaurant.name; 
+
+
+
+--List all the reviews along with the restaurant, and the reviewer's name. The result should have the restaurant name, the review text, and the reviewer name. Hint: you will need to do a three-way join - i.e. joining all three tables together.
+
+-- SELECT review, restaurant.name as restaurant_name, reviewer.name as reviewer_name
+-- FROM review
+-- INNER JOIN restaurant ON review.restaurant_id = restaurant.id
+-- INNER JOIN reviewer ON review.reviewer_id = reviewer.id;
+
+
+
+--Get the average stars given by each reviewer. (reviewer name, average star rating)
+
+-- SELECT AVG(stars) as average_star_rating, reviewer.name as reviewer_name
+-- FROM review
+-- INNER JOIN reviewer ON review.reviewer_id = reviewer.id
+-- GROUP BY reviewer.name;
+
+
+
+--Get the lowest star rating given by each reviewer. (reviewer name, lowest star rating)
